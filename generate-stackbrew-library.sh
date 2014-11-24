a85454871e28230ad36e27fcd80cdbb8a51784c0
Joe Ferguson <joe@infosiftr.com>
2014-11-24 15:36:06 -0800

    Fix `git log` line

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -16,7 +16,7 @@ url='git://github.com/docker-library/mariadb'
 echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
 
 for version in "${versions[@]}"; do
-	commit="$(git log -1 --format='format:%H' "$version")"
+	commit="$(git log -1 --format='format:%H' -- "$version")"
 	fullVersion="$(grep -m1 'ENV MARIADB_VERSION ' "$version/Dockerfile" | cut -d' ' -f3 | cut -d+ -f1)"
 	versionAliases=( $fullVersion $version ${aliases[$version]} )
 	
