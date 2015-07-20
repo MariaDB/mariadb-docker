4fb7e0e3a475acbb8e01ceefb67764bb185d4520
Tianon Gravi <admwiggin@gmail.com>
2015-07-20 15:28:26 -0700

    Update 10.0 to jessie

--- update.sh
+++ update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+declare -A suites=(
+	[5.5]='wheezy'
+)
+defaultSuite='jessie'
+
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( "$@" )
@@ -11,11 +16,20 @@ versions=( "${versions[@]%/}" )
 
 
 for version in "${versions[@]}"; do
-	fullVersion="$(curl -sSL "http://ftp.osuosl.org/pub/mariadb/repo/$version/debian/dists/wheezy/main/binary-amd64/Packages" | grep -m1 -A10 "^Package: mariadb-server\$" | grep -m1 '^Version: ' | cut -d' ' -f2)"
+	suite="${suites[$version]:-$defaultSuite}"
+	fullVersion="$(curl -sSL "http://ftp.osuosl.org/pub/mariadb/repo/$version/debian/dists/$suite/main/binary-amd64/Packages" |tac|tac| grep -m1 -A10 "^Package: mariadb-server\$" | grep -m1 '^Version: ' | cut -d' ' -f2)"
+	if [ -z "$fullVersion" ]; then
+		echo >&2 "warning: cannot find $version in $suite"
+		continue
+	fi
 	(
 		set -x
 		cp docker-entrypoint.sh Dockerfile.template "$version/"
 		mv "$version/Dockerfile.template" "$version/Dockerfile"
-		sed -i 's/%%MARIADB_MAJOR%%/'$version'/g; s/%%MARIADB_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
+		sed -i '
+			s/%%SUITE%%/'"$suite"'/g;
+			s/%%MARIADB_MAJOR%%/'"$version"'/g;
+			s/%%MARIADB_VERSION%%/'"$fullVersion"'/g;
+		' "$version/Dockerfile"
 	)
 done
