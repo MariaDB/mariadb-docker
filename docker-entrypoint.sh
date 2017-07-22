e6abac797c9de2ecf59b3a16a6eb1ded68ed9f55
Daniel Niedergesäß <dng-dev@users.noreply.github.com>
2017-07-22 22:01:46 +0200

    Update docker-entrypoint.sh
    I needed the possibility to import dump.sql.tar.gz

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -169,10 +169,11 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 		echo
 		for f in /docker-entrypoint-initdb.d/*; do
 			case "$f" in
-				*.sh)     echo "$0: running $f"; . "$f" ;;
-				*.sql)    echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
-				*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
-				*)        echo "$0: ignoring $f" ;;
+				*.sh)          echo "$0: running $f"; . "$f" ;;
+				*.sql)         echo "$0: running $f"; "${mysql[@]}" < "$f"; echo ;;
+				*.sql.gz)      echo "$0: running $f"; gunzip -c "$f" | "${mysql[@]}"; echo ;;
+				*.sql.tar.gz)  echo "$0: running $f"; tar xzOf "$f" | "${mysql[@]}"; echo ;;
+				*)             echo "$0: ignoring $f" ;;
 			esac
 			echo
 		done
