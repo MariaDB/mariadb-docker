94e05986d626a473f83ccc5e8a3ac2dd54403b8c
Daniel Black <daniel@mariadb.org>
2021-05-27 17:02:45 +1000

    allow mariadbd as entrypoint name
    Equivalent as the mysqld name.

--- 10.6/docker-entrypoint.sh
+++ 10.6/docker-entrypoint.sh
@@ -350,7 +350,7 @@ _main() {
 	fi
 
 	# skip setup if they aren't running mysqld or want an option that stops mysqld
-	if [ "$1" = 'mysqld' ] && ! _mysql_want_help "$@"; then
+	if [ "$1" = 'mariadbd' ] || [ "$1" = 'mysqld' ] && ! _mysql_want_help "$@"; then
 		mysql_note "Entrypoint script for MariaDB Server ${MARIADB_VERSION} started."
 
 		mysql_check_config "$@"
