cc9c2f513d14e19724b59af59f51969e89bc85dd
Daniel Black <daniel@mariadb.org>
2021-08-10 11:52:08 +1000

    test: jemalloc test multiarch + jump to test
    Check we pass a image to the test script.
    
    Made jemalloc test into a multiarch/cross execution compatible test
    
    Added logic to jump to a particular test

--- .test/run.sh
+++ .test/run.sh
@@ -3,7 +3,15 @@ set -eo pipefail
 
 dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+if [ $# -eq 0 ]
+then
+	echo "An image argument is required" >&2
+	exit 1
+fi
+
 image="$1"
+
+architecture=$(docker image inspect --format '{{.Architecture}}' "$image")
 
 killoff()
 {
@@ -69,13 +77,22 @@ mariadbclient_unix() {
 		"$@"
 }
 
+case ${2:-all} in
+	all|required_password)
+
 echo -e "Test: expect Failure - none of MYSQL_ALLOW_EMPTY_PASSWORD, MYSQL_RANDOM_ROOT_PASSWORD, MYSQL_ROOT_PASSWORD\n"
 
 cname="mariadb-container-fail-to-start-options-$RANDOM-$RANDOM"
 docker run --name "$cname" --rm "$image" 2>&1 && die "$cname should fail with unspecified option"
 
-echo -e "Test: MYSQL_ALLOW_EMPTY_PASSWORD Implementation is non-empty value so this should fail\n"
+	;&
+	mysql_allow_empty_password_is_empty)
+
+echo -e "Test: MYSQL_ALLOW_EMPTY_PASSWORD Implementation is empty value so this should fail\n"
 docker run  --rm  --name "$cname" -e MYSQL_ALLOW_EMPTY_PASSWORD  "$image" || echo 'expected failure of empty MYSQL_ALLOW_EMPTY_PASSWORD'
+
+	;&
+	mysql_allow_empty_password_is_clean)
 
 echo -e "Test: MYSQL_ALLOW_EMPTY_PASSWORD and defaults to clean environment\n"
 
@@ -89,12 +106,18 @@ otherusers=$(mariadbclient -u root --skip-column-names -Be "select user,host fro
 [ "$otherusers" != '' ] && die "unexpected users $otherusers"
 killoff
 
+	;&
+	mysql_root_password_is_set)
+
 echo -e "Test: MYSQL_ROOT_PASSWORD\n"
 
 runandwait -e MYSQL_ROOT_PASSWORD=examplepass "${image}"
 mariadbclient -u root -pexamplepass -e 'select current_user()'
 mariadbclient -u root -pwrongpass -e 'select current_user()' || echo 'expected failure' 
 killoff 
+
+	;&
+	mysql_random_password_is_complex)
 
 echo -e "Test: MYSQL_RANDOM_ROOT_PASSWORD, needs to satisify minimium complexity of simple-password-check plugin\n"
 
@@ -104,6 +127,9 @@ pass=$(docker logs $cid | grep 'GENERATED ROOT PASSWORD' 2>&1)
 pass=${pass#*GENERATED ROOT PASSWORD: }
 mariadbclient -u root -p"${pass}" -e 'select current_user()'
 killoff
+
+	;&
+	mysql_random_password_is_different)
 
 echo -e "Test: second instance of MYSQL_RANDOM_ROOT_PASSWORD has a different password\n"
 
@@ -116,6 +142,9 @@ killoff
 
 [ "$pass" = "$newpass" ] && die "highly improbable - two consequitive passwords are the same"
 
+	;&
+	mysql_root_host_sets_host)
+
 echo -e "Test: MYSQL_ROOT_HOST\n"
 
 runandwait -e  MYSQL_ALLOW_EMPTY_PASSWORD=1  -e MYSQL_ROOT_HOST=apple "${image}" 
@@ -123,12 +152,18 @@ ru=$(mariadbclient_unix --skip-column-names -B -u root -e 'select user,host from
 [ "${ru}" = '' ] && die 'root@apple not created'
 killoff
 
+	;&
+	complex_passwords)
+
 echo -e "Test: complex passwords\n"
 
 runandwait -e MYSQL_USER=bob -e MYSQL_PASSWORD=$'\n \' \n' -e MYSQL_ROOT_PASSWORD=$'\n\'\\aa-\x09-zz"_%\n' "${image}"
 mariadbclient_unix --skip-column-names -B -u root -p$'\n\'\\aa-\x09-zz"_%\n' -e 'select 1'
 mariadbclient_unix --skip-column-names -B -u bob -p$'\n \' \n' -e 'select 1'
 killoff
+
+	;&
+	mysql_initdb_skip_tzinfo_empty)
 
 echo -e "Test: MYSQL_INITDB_SKIP_TZINFO='' should still load timezones\n"
 
@@ -138,12 +173,18 @@ tzcount=$(mariadbclient --skip-column-names -B -u root -e "SELECT COUNT(*) FROM 
 [ "$(mariadbclient --skip-column-names -B -u root -e 'SELECT @@time_zone')" != "Europe/Berlin" ] && die "Didn't set timezone to Berlin"
 killoff
 
+	;&
+	mysql_initdb_skip_tzinfo_no_empty)
+
 echo -e "Test: MYSQL_INITDB_SKIP_TZINFO=1 should not load timezones\n"
 
 runandwait -e MYSQL_INITDB_SKIP_TZINFO=1 -e MYSQL_ALLOW_EMPTY_PASSWORD=1 "${image}"
 tzcount=$(mariadbclient --skip-column-names -B -u root -e "SELECT COUNT(*) FROM mysql.time_zone")
 [ "${tzcount}" = '0' ] || die "timezones shouldn't be loaded - found ${tzcount}"
 killoff
+
+	;&
+	secrets_via_file)
 
 echo -e "Test: Secrets _FILE vars shoud be same as env directly\n"
 
@@ -171,6 +212,9 @@ creation=$(mariadbclient --skip-column-names -B -u ron -pscappers -P 3306 --prot
 killoff
 rm -rf "${secretdir}"
 
+	;&
+	docker_entrypint_initdb)
+
 echo -e "Test: docker-entrypoint-initdb.d Initialization order is correct and processed\n"
 
 initdb=$(mktemp -d)
@@ -194,6 +238,9 @@ killoff
 rm -rf "${initdb}"
 
 
+	;&
+	prefer_mariadb_names)
+
 echo -e "Test: when provided with MYSQL_ and MARIADB_ names, Prefer MariaDB names\n"
 
 runandwait -e MARIADB_ROOT_PASSWORD=examplepass -e MYSQL_ROOT_PASSWORD=mysqlexamplepass "${image}"
@@ -201,9 +248,15 @@ mariadbclient -u root -pexamplepass -e 'select current_user()'
 mariadbclient -u root -pwrongpass -e 'select current_user()' || echo 'expected failure of wrong password'
 killoff
 
-echo -e "Test: MARIADB_ALLOW_EMPTY_ROOT_PASSWORD Implementation is non-empty value so this should fail\n"
+	;&
+	mariadb_allow_empty_root_password_empty)
+
+echo -e "Test: MARIADB_ALLOW_EMPTY_ROOT_PASSWORD Implementation is empty value so this should fail\n"
 
 docker run  --rm  --name "$cname" -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD "$image" || echo 'expected failure MARIADB_ALLOW_EMPTY_ROOT_PASSWORD is empty'
+
+	;&
+	mariadb_allow_empty_root_password_not_empty)
 
 echo -e "Test: MARIADB_ALLOW_EMPTY_ROOT_PASSWORD\n"
 
@@ -218,12 +271,18 @@ otherusers=$(mariadbclient -u root --skip-column-names -Be "select user,host fro
 [ "$otherusers" != '' ] && die "unexpected users $otherusers"
 killoff
 
+	;&
+	mariadb_root_password_is_set)
+
 echo -e "Test: MARIADB_ROOT_PASSWORD\n"
 
 runandwait -e MARIADB_ROOT_PASSWORD=examplepass "${image}"
 mariadbclient -u root -pexamplepass -e 'select current_user()'
 mariadbclient -u root -pwrongpass -e 'select current_user()' || echo 'expected failure' 
 killoff 
+
+	;&
+	mariadb_root_password_is_complex)
 
 echo -e "Test: MARIADB_RANDOM_ROOT_PASSWORD, needs to satisify minimium complexity of simple-password-check plugin\n"
 
@@ -234,7 +293,10 @@ pass=${pass#*GENERATED ROOT PASSWORD: }
 mariadbclient -u root -p"${pass}" -e 'select current_user()'
 killoff
 
-echo -e "Test: second instance of MARIADB_RANDOM_ROOT_PASSWORD has a differnet password\n"
+	;&
+	mariadb_root_password_is_different)
+
+echo -e "Test: second instance of MARIADB_RANDOM_ROOT_PASSWORD has a different password\n"
 
 runandwait -e MARIADB_RANDOM_ROOT_PASSWORD=1 "${image}" --plugin-load-add=simple_password_check
 newpass=$(docker logs $cid  2>&1 | grep 'GENERATED ROOT PASSWORD')
@@ -245,6 +307,9 @@ killoff
 
 [ "$pass" = "$newpass" ] && die "highly improbable - two consequitive random passwords are the same"
 
+	;&
+	mariadb_root_host_sets_host)
+
 echo -e "Test: MARIADB_ROOT_HOST\n"
 
 runandwait -e  MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=1  -e MARIADB_ROOT_HOST=apple "${image}"
@@ -252,12 +317,34 @@ ru=$(mariadbclient_unix --skip-column-names -B -u root -e 'select user,host from
 [ "${ru}" = '' ] && die 'root@apple not created'
 killoff
 
+	;&
+	mariadb_initdb_skip_tzinfo_empty)
+
 echo -e "Test: MARIADB_INITDB_SKIP_TZINFO=''\n"
 
 runandwait -e MARIADB_INITDB_SKIP_TZINFO= -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=1 "${image}"
 tzcount=$(mariadbclient --skip-column-names -B -u root -e "SELECT COUNT(*) FROM mysql.time_zone")
 [ "${tzcount}" = '0' ] && die "should exist timezones"
+
+# note uses previous instance
+echo -e "Test: default configuration items are present\n"
+arg_expected=0
+docker exec -i $cid my_print_defaults --mysqld |
+	{
+	while read line
+	do
+		case $line in
+		--skip-host-cache|--skip-name-resolve)
+			echo $line found
+			(( arg_expected++ )) || : ;;
+		esac
+	done
+	[ $arg_expected -eq 2 ] || die "expected both skip-host-cache and skip-name-resolve"
+}
 killoff
+
+	;&
+	mariadb_initdb_skip_tzinfo_not_empty)
 
 echo -e "Test: MARIADB_INITDB_SKIP_TZINFO=1\n"
 
@@ -266,30 +353,37 @@ tzcount=$(mariadbclient --skip-column-names -B -u root -e "SELECT COUNT(*) FROM 
 [ "${tzcount}" = '0' ] || die "timezones shouldn't be loaded - found ${tzcount}"
 killoff
 
-# lazy test because ubuntu arch isn't always the same as uname -m
-if [ $(uname -m) = 'x86_64' ]
+	;&
+	jemalloc)
+
+case "$architecture" in
+	amd64)
+		debarch=x86_64 ;;
+	arm64)
+		debarch=aarch64 ;;
+	ppc64le)
+		debarch=powerpc64le ;;
+	s390x|i386)
+		debarch=$architecture ;;
+esac
+if [ -n "$debarch" ]
 then
 	echo -e "Test: jemalloc preload\n"
-	runandwait -e LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libjemalloc.so.1 /usr/lib/x86_64-linux-gnu/libjemalloc.so.2" -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=1 "${image}"
+	runandwait -e LD_PRELOAD="/usr/lib/$debarch-linux-gnu/libjemalloc.so.1 /usr/lib/$debarch-linux-gnu/libjemalloc.so.2" -e MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=1 "${image}"
 	docker exec -i $cid gosu mysql /bin/grep 'jemalloc' /proc/1/maps || die "expected to preload jemalloc"
 
 
-	echo -e "Test: default configuration items are present\n"
-	arg_expected=0
-	docker exec -i $cid my_print_defaults --mysqld |
-		{
-		while read line
-		do
-			case $line in
-			--skip-host-cache|--skip-name-resolve)
-				echo $line found
-				(( arg_expected++ )) || : ;;
-			esac
-		done
-		[ $arg_expected -eq 2 ] || die "expected both skip-host-cache and skip-name-resolve"
-	}
-
-
 	killoff
+else
+	echo -e "Test: jemalloc skipped - unknown arch '$architecture'\n"
 fi
 
+# Insert new tests above by copying the comments below
+#	;&
+#	THE_TEST_NAME)
+
+	;;
+	*)
+	echo "Test $2 not found" >&2
+	exit 1
+esac
