f99ab8b68155a6c030803c598b507e047f62313f
Daniel Black <daniel@mariadb.org>
2022-02-04 12:15:33 +1100

    skip innodb buffer pool loads/saves on temporary startup/shutdown
    This saves a small bit of IO, more so with mariadb-upgrade, and
    none of the IO/memory acheives much gain in theses temporary start
    situtations.
    
    Also by skipping on the temporary starts, the innodb_buffer_pool_loaded
    option in the healthcheck script only gains the "loaded" status
    on the final startup in the entrypoint.

--- 10.3/docker-entrypoint.sh
+++ 10.3/docker-entrypoint.sh
@@ -120,7 +120,8 @@ mysql_get_config() {
 
 # Do a temporary startup of the MariaDB server, for init purposes
 docker_temp_server_start() {
-	"$@" --skip-networking --default-time-zone=SYSTEM --socket="${SOCKET}" --wsrep_on=OFF --skip-log-bin &
+	"$@" --skip-networking --default-time-zone=SYSTEM --socket="${SOCKET}" --wsrep_on=OFF --skip-log-bin \
+		--loose-innodb_buffer_pool_load_at_startup=0 --loose-innodb_buffer_pool_dump_at_shutdown=0 &
 	mysql_note "Waiting for server startup"
 	# only use the root password if the database has already been initializaed
 	# so that it won't try to fill in a password file when it hasn't been set yet
