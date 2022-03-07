42011e6e66108f844939df5fda27b1b49ea95a85
Daniel Black <daniel@mariadb.org>
2022-03-07 15:59:10 +1100

    Revert "healthcheck: add --su-mariadb"
    This reverts commit 02a42958d32bb00c104a35695269231367b06307.

--- 10.8/healthcheck.sh
+++ 10.8/healthcheck.sh
@@ -252,7 +252,6 @@ while [ $# -gt 0 ]; do
 			shift
 			exec gosu "$u" "${BASH_SOURCE[0]}" "$@"
 			;;
-		--su-mariadb) ;& # compatible with prev mistake
 		--su-mysql)
 			shift
 			exec gosu mysql "${BASH_SOURCE[0]}" "$@"
