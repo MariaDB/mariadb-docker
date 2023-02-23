afd4731208fe075c994fb038c743c437cd511da4
Daniel Black <daniel@mariadb.org>
2023-02-23 18:56:44 +1100

    11.0 update + healthcheck.sh typo

--- 11.0/healthcheck.sh
+++ 11.0/healthcheck.sh
@@ -183,7 +183,7 @@ replication()
 
 # mariadbupgrade
 #
-# Test the lock on the file /var/lib/mariadb_upgrade_info
+# Test the lock on the file /var/lib/mariadb/mariadb_upgrade_info
 # https://jira.mariadb.org/browse/MDEV-27068
 mariadbupgrade()
 {
