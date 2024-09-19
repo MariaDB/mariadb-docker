e50c40eea0edb101fddd63e51994654dd5ec27e0
Daniel Black <daniel@mariadb.org>
2024-09-19 16:43:28 +1000

    Replication fix - updates for 11.6

--- 11.6/docker-entrypoint.sh
+++ 11.6/docker-entrypoint.sh
@@ -121,6 +121,7 @@ mysql_get_config() {
 docker_temp_server_start() {
 	"$@" --skip-networking --default-time-zone=SYSTEM --socket="${SOCKET}" --wsrep_on=OFF \
 		--expire-logs-days=0 \
+		--skip-slave-start \
 		--loose-innodb_buffer_pool_load_at_startup=0 \
 		--skip-ssl --ssl-cert='' --ssl-key='' --ssl-ca='' \
 		&
@@ -460,7 +461,6 @@ docker_setup_db() {
 	# To create replica user
 	local createReplicaUser=
 	local changeMasterTo=
-	local startReplica=
 	if  [ -n "$MARIADB_REPLICATION_USER" ] ; then
 		if [ -z "$MARIADB_MASTER_HOST" ]; then
 			# on master
@@ -473,7 +473,6 @@ docker_setup_db() {
 			# SC cannot follow how MARIADB_MASTER_PORT is assigned a default value.
 			# shellcheck disable=SC2153
 			changeMasterTo="CHANGE MASTER TO MASTER_HOST='$MARIADB_MASTER_HOST', MASTER_USER='$MARIADB_REPLICATION_USER', MASTER_PASSWORD='$rplPasswordEscaped', MASTER_PORT=$MARIADB_MASTER_PORT, MASTER_CONNECT_RETRY=10;"
-			startReplica="START REPLICA;"
 		fi
 	fi
 
@@ -504,7 +503,6 @@ docker_setup_db() {
 		${userGrants}
 
 		${changeMasterTo}
-		${startReplica}
 	EOSQL
 }
 
@@ -600,8 +598,7 @@ docker_mariadb_upgrade() {
 	fi
 	mysql_note "Starting temporary server"
 	docker_temp_server_start "$@" --skip-grant-tables \
-		--loose-innodb_buffer_pool_dump_at_shutdown=0 \
-		--skip-slave-start
+		--loose-innodb_buffer_pool_dump_at_shutdown=0
 	mysql_note "Temporary server started."
 
 	docker_mariadb_backup_system
@@ -626,8 +623,7 @@ EOSQL
 			# need a restart as FLUSH PRIVILEGES isn't reversable
 			mysql_note "Restarting temporary server for upgrade"
 			docker_temp_server_start "$@" --skip-grant-tables \
-				--loose-innodb_buffer_pool_dump_at_shutdown=0 \
-				--skip-slave-start
+				--loose-innodb_buffer_pool_dump_at_shutdown=0
 		else
 			return 0
 		fi
