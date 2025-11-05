215b7aed18a72e816b6b3aa6e0b8219bb3945f94
Daniel Black <daniel@mariadb.org>
2025-11-05 18:17:17 +1100

    mysql 8 migration - handle script as command
    This allows the container to finish once the dump as been made
    to allow the next migration to start.
    
    With this, we've used the root user to allow changing of ownership
    of the mysqldump volume.
    
    Because of MDEV-20912 being resolved we don't need to manually change
    collations.

--- examples/migration-8.0/dump-mysql.sh
+++ examples/migration-8.0/dump-mysql.sh
@@ -3,7 +3,9 @@
 echo 'MariaDB service started.'
 # Run your commands and exit container
 whoami # mysql
-# sh -c "chown -R mysql:mysql /etc/dump" # Operation permitted
+if [ "$(whoami)" = root ]; then
+    chown -R mysql:mysql /etc/dump
+fi
 echo 'Remove files if exist'
 files=("mysql-dump-data.sql.zst" "mysql-dump-users.sql.zst" "mysql-dump-stats.sql.zst" "mysql-dump-tzs.sql.zst")
 for fileName in "${files[@]}"; do
@@ -14,15 +16,15 @@ for fileName in "${files[@]}"; do
 done
 echo 'Dump and compress MySQL data with changed collation ...'
 fileName="mysql-dump-data.sql.zst"
-sh -c "mariadb-dump -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} ${MARIADB_DB} | sed 's/utf8mb4_0900/uca1400/g' | zstd > /etc/dump/${fileName}"
+gosu mysql sh -c "mariadb-dump --ssl-verify-server-cert=OFF -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} ${MARIADB_DB} | zstd > /etc/dump/${fileName}"
 echo 'Dump and compress MySQL users ...'
 fileName="mysql-dump-users.sql.zst"
-sh -c "mariadb-dump -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} --system=users | zstd > /etc/dump/${fileName}"
+gosu mysql sh -c "mariadb-dump --ssl-verify-server-cert=OFF -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} --system=users | zstd > /etc/dump/${fileName}"
 echo 'Dump and compress MySQL stats ...'
 fileName="mysql-dump-stats.sql.zst"
-sh -c "mariadb-dump -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} --system=stats | zstd > /etc/dump/${fileName}"
+gosu mysql sh -c "mariadb-dump --ssl-verify-server-cert=OFF -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} --system=stats | zstd > /etc/dump/${fileName}"
 echo 'Dump and compress MySQL timezones ...'
 fileName="mysql-dump-tzs.sql.zst"
-sh -c "mariadb-dump -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} --system=timezones | zstd > /etc/dump/${fileName}"
-echo 'Show MySQL 8.0 create user'
-sh -c "mariadb -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} -e 'SELECT @@print_identified_with_as_hex; show create user current_user();'"
+gosu mysql sh -c "mariadb-dump --ssl-verify-server-cert=OFF -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} --system=timezones | zstd > /etc/dump/${fileName}"
+echo 'Show MySQL 8.x create user'
+gosu mysql sh -c "mariadb --ssl-verify-server-cert=OFF -h${MYSQL_CONT_NAME} -uroot -p${MARIADB_ROOT_PASSWORD} -e 'SELECT @@print_identified_with_as_hex; show create user current_user();'"
